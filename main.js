7f6d373288c39b43eaffac9f9b3212fd3bc4136b
Bianca Mazzi <bianca.mazzi@hotmail.com>
2020-07-06 23:22:52 -0300

    atualizacoes finais

---
+++ main.js
@@ -0,0 +1,64 @@
+$('.depoimento-carrousel').slick({
+    dots: true,
+    infinite: true,
+    arrows: false,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+    ]
+});
+$('.ecommerce-carrousel').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+    ]
+});
